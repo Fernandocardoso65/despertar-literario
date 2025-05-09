099fee02abfab9cdf012d06e4148793bf10448e3
Fernando Cardoso <101380198+Fernandocardoso65@users.noreply.github.com>
2025-05-09 08:18:20 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,36 +4,35 @@ const urlsToCache = [
   '/index.html',
   '/segunda_tela.html',
   '/terceira_tela.html',
-  '/icon.png',
-  '/logo.jpg',
-  '/background-night.jpg',
-  '/background-day.jpg'
+  'https://images.unsplash.com/photo-1519125323398-675f1f4d271f?ixlib=rb-4.0.3&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1507521628349-dee6b5761201?ixlib=rb-4.0.3&auto=format&fit=crop&w=1350&q=80',
+  'https://i.imgur.com/eKNOJwl.jpeg'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => {
+        return response || fetch(event.request).catch(() => caches.match('/index.html'));
+      })
   );
 });
 
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
       );
     })
   );

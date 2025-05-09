639e8fbf3c8290a1a34cf82197fe8fa605a46ddf
Fernando Cardoso <101380198+Fernandocardoso65@users.noreply.github.com>
2025-05-09 15:03:28 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,23 @@
-const CACHE_NAME = 'despertar-literario-v3';
-    const urlsToCache = [
-      '/',
-      '/index.html',
-      '/icon-192.png',
-      '/icon-512.png'
-    ];
+const CACHE_NAME = 'despertar-literario-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/quiz.html',
+    '/segunda_tela.html',
+    '/terceira_tela.html',
+    '/icon-192.png'
+];
 
-    self.addEventListener('install', (event) => {
-      console.log('Service Worker: Instalando...');
-      event.waitUntil(
+self.addEventListener('install', (event) => {
+    event.waitUntil(
         caches.open(CACHE_NAME)
-          .then((cache) => {
-            console.log('Service Worker: Cache aberto, adicionando arquivos...');
-            return cache.addAll(urlsToCache).catch((err) => {
-              console.error('Service Worker: Erro ao cachear arquivos:', err);
-            });
-          })
-      );
-      self.skipWaiting();
-    });
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
 
-    self.addEventListener('fetch', (event) => {
-      console.log('Service Worker: Fetching', event.request.url);
-      event.respondWith(
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
         caches.match(event.request)
-          .then((response) => {
-            if (response) {
-              console.log('Service Worker: Retornando do cache:', event.request.url);
-              return response;
-            }
-            console.log('Service Worker: Buscando na rede:', event.request.url);
-            return fetch(event.request).catch(() => {
-              console.log('Service Worker: Rede falhou, retornando fallback');
-              return caches.match('/index.html');
-            });
-          })
-      );
-    });
-
-    self.addEventListener('activate', (event) => {
-      console.log('Service Worker: Ativando...');
-      event.waitUntil(
-        caches.keys().then((cacheNames) => {
-          return Promise.all(
-            cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
-              .map((cacheName) => {
-                console.log('Service Worker: Removendo cache antigo:', cacheName);
-                return caches.delete(cacheName);
-              })
-          );
-        })
-      );
-      self.clients.claim();
-    });
+            .then((response) => response || fetch(event.request))
+    );
+});
